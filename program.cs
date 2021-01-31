74bd2990860e38711143a03a5dd428a258cebcd4
Frxq15 <51741750+Frxq15@users.noreply.github.com>
2021-01-31 17:47:10 +0000

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace EventManager
+{
+    //https://prod.liveshare.vsengsaas.visualstudio.com/join?C1DF495EA27950520672D8848E5FC341AF72
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.Login();
+        }
+    }
+}
