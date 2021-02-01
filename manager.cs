6dc7186847f20d4c74252ad5c758f2d59019fb2d
Frxq15 <51741750+Frxq15@users.noreply.github.com>
2021-02-01 22:22:27 +0000

    menu and formatting fixes

--- manager.cs
+++ manager.cs
@@ -23,8 +23,8 @@ namespace EventManager
             switch (username.ToLower())
             {
                 case "demouser":
-                    string password = Console.ReadLine();
                     Console.WriteLine("Please enter your password:");
+                    string password = Console.ReadLine();
 
                     while (!password.Equals("password"))
                     {
@@ -88,7 +88,9 @@ namespace EventManager
             Console.WriteLine("\t\t3: Delete an event");
             Console.WriteLine("\t\t4: Book tickets");
             Console.WriteLine("\t\t5: Display event list");
-            Console.WriteLine("\t\t6: Display transaction log\n");
+            Console.WriteLine("\t\t5: View self purchased tickets");
+            Console.WriteLine("\t\t7: Display transaction log");
+            Console.WriteLine("\nType 'logout' to logout of your account\n");
 
             string option = Console.ReadLine();
 
@@ -102,51 +104,92 @@ namespace EventManager
 
                 case "2":
                     Console.WriteLine("Option 2 Selected.");
-                    handler.UpdateEvent(username);
+                    handler.UpdateEventInfo(username);
                     return true;
 
                 case "3":
                     Console.WriteLine("Option 3 Selected.");
+                    handler.DeleteEvent(username);
                     return true;
 
                 case "4":
                     Console.WriteLine("Option 4 Selected.");
+                    handler.BookTickets(username);
                     return true;
 
                 case "5":
                     Console.WriteLine("Option 5 Selected.");
+                    handler.PrintEvents(username);
                     return true;
 
                 case "6":
                     Console.WriteLine("Option 6 Selected.");
+                    handler.ViewOwnTickets(username);
+                    return true;
+
+                case "7":
+                    Console.WriteLine("Option 7 Selected.");
                     handler.DisplayLog(username);
                     return true;
 
+                case "logout":
+                case "exit":
+                    Console.Clear();
+                    Login();
+                    return true;
+
                 default:
+                    Console.WriteLine("Invalid option selected.");
+                    System.Threading.Thread.Sleep(1000);
+                    AdminMenu(username);
                     return true;
             }
         }
         public bool UserMenu(string username)
         {
+            EventsHandler handler = new EventsHandler();
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Clear();
             Console.WriteLine("");
             Console.WriteLine("\t\t[+] User Main Menu [+]");
             Console.WriteLine("\tPlease select from one of the following options");
-            Console.WriteLine("\t\t  [1, 2, 3, 4, 5, 6]\n");
+            Console.WriteLine("\t\t      [1, 2, 3]\n");
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("\t\t1: Book tickets");
-            Console.WriteLine("\t\t2: Display event list");
+            Console.WriteLine("\t\t2: View self purchased tickets");
+            Console.WriteLine("\t\t3: Display event list");
+            Console.WriteLine("\nType 'logout' to logout of your account\n");
 
             string option = Console.ReadLine();
 
             switch (option)
             {
                 case "1":
+                    Console.WriteLine("Option 1 Selected.");
+                    handler.BookTickets(username);
+                    return true;
+
+                case "2":
+                    Console.WriteLine("Option 2 Selected.");
+                    handler.ViewOwnTickets(username);
+                    return true;
+
+                case "3":
+                    Console.WriteLine("Option 3 Selected.");
+                    handler.PrintEvents(username);
+                    return true;
+
+                case "logout":
+                case "exit":
+                    Console.Clear();
+                    Login();
                     return true;
 
                 default:
+                    Console.WriteLine("Invalid option selected.");
+                    System.Threading.Thread.Sleep(1000);
+                    UserMenu(username);
                     return true;
             }
         }
