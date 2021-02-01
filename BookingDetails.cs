1a36652a32e32add4c02f6fedb57e5c0561423f8
Frxq15 <51741750+Frxq15@users.noreply.github.com>
2021-02-01 23:53:03 +0000

    Create BookingDetails.cs

---
+++ BookingDetails.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManager
+{
+    public class BookingDetails
+    {
+        public string CustomerName { get; set; }
+        public string PurchasedBy { get; set; }
+        public string CustomerAddress { get; set; }
+        public int NumberTickets { get; set; }
+    }
+}
