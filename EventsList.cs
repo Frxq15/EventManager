39ae147ef853b9878c9dfbcf9599c10ef753b7b1
Frxq15 <51741750+Frxq15@users.noreply.github.com>
2021-02-01 11:50:09 +0000

    Create EventsList.cs

---
+++ EventsList.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManager
+{
+    public class EventDetails
+    {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public int AmountTickets { get; set; }
+        public float PricePerTicket { get; set; }
+        public string DateTime { get; set; }
+    }
+}
