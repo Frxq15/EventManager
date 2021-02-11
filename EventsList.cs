a1e2d801321c90d193725fc9c12e3b419df5490b
Tom-Loader <42319836+Tom-Loader@users.noreply.github.com>
2021-02-11 15:42:29 +0000

    Update EventsList.cs

--- EventsList.cs
+++ EventsList.cs
@@ -4,12 +4,13 @@ using System.Text;
 
 namespace EventManager
 {
-    public class EventDetails
+    public class EventDetails 
     {
         public int EventID { get; set; }
         public string EventName { get; set; }
         public int AmountTickets { get; set; }
         public float PricePerTicket { get; set; }
         public string DateTime { get; set; }
+        public List<BookingDetails> BookingDetails = new List<BookingDetails>();
     }
 }
