558c4f1e4cbbd1903810ac2cdb24b8638b9ff236
Frxq15 <51741750+Frxq15@users.noreply.github.com>
2021-03-11 17:50:43 +0000

    Update program.cs
    initial commit

--- program.cs
+++ program.cs
@@ -2,7 +2,6 @@ using System;
 
 namespace EventManager
 {
-    //https://prod.liveshare.vsengsaas.visualstudio.com/join?C1DF495EA27950520672D8848E5FC341AF72
     class Program
     {
         static void Main(string[] args)
